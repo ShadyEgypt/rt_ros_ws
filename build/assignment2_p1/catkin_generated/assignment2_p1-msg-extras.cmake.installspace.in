4f5e7a43b901527fc258f98199fd30dca6e75c0d
ShadyEgypt <shadyrafat60@gmail.com>
2025-02-11 20:42:25 +0100

    remove build files

--- build/assignment2_p1/catkin_generated/assignment2_p1-msg-extras.cmake.installspace.in
+++
@@ -1,2 +0,0 @@
-set(assignment2_p1_MESSAGE_FILES "msg/RobotStatus.msg")
-set(assignment2_p1_SERVICE_FILES "srv/GetLastTarget.srv")
--- build/assignment2_p1/catkin_generated/installspace/assignment2_p1-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
-
-_prepend_path("${assignment2_p1_DIR}/.." "msg" assignment2_p1_MSG_INCLUDE_DIRS UNIQUE)
-set(assignment2_p1_MSG_DEPENDENCIES std_msgs;actionlib_msgs;geometry_msgs)
